9d7ce98dc089159f7879dd8f02467bf135727bac
AmirHosseinBaderan <amir0hossin0pr@gmail.com>
2021-01-20 00:21:47 +0330

    Change Account Controller

--- FSI.Server/Api/AccountApiController.cs
+++ FSI.Server/Api/AccountApiController.cs
@@ -1,8 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Fri2Ends.Identity.Services.Repository;
+using Fri2Ends.Identity.Services.Srevices;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace FSI.Server.Api
