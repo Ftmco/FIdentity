b10610a8b895a87d60cf5bc0806a8d19f6c6f8b9
AmirHosseinBaderan <amir0hossin0pr@gmail.com>
2021-01-20 23:16:54 +0330

    Send Mail To Meny Users

--- Services/Tools/EmailSender.cs
+++ Services/Tools/EmailSender.cs
@@ -10,24 +10,35 @@ using System.Threading.Tasks;
 /// </summary>
 public class EmailSender
 {
+
+    /// <summary>
+    /// Send Email Async
+    /// </summary>
+    /// <param name="emailModel">Email Model</param>
+    /// <returns>
+    /// String Status 
+    /// Success Or Exeption Message
+    /// </returns>
     public static async Task<string> Send(SendEmailModel emailModel)
     {
         return await Task.Run(async () =>
         {
             try
             {
-                SmtpClient SmtpClient = new SmtpClient(emailModel.SmtpServer);
+                SmtpClient SmtpClient = new SmtpClient(emailModel.Host);
 
                 MailMessage mail = new MailMessage
                 {
-                    From = new MailAddress("friendstmco@outlook.com", emailModel.DisplayName),
+                    From = new MailAddress(emailModel.Address, emailModel.DisplayName),
                     IsBodyHtml = true,
                     Subject = emailModel.Subject,
                     Body = emailModel.Body,
                 };
 
-                mail.To.Add(emailModel.To);
-
+                foreach (var item in emailModel.To)
+                {
+                    mail.To.Add(item);
+                }
                 //mail.Subject = Subject;
                 //mail.Body = Body;
                 //mail.IsBodyHtml = true;
@@ -48,11 +59,12 @@ public class EmailSender
 
     public record SendEmailModel
     {
-        public string To { get; set; }
+        public string Address { get; set; }
+        public IList<string> To { get; set; }
         public string Subject { get; set; }
         public string Body { get; set; }
         public string DisplayName { get; set; }
-        public string SmtpServer { get; set; }
+        public string Host { get; set; }
         public string EmailFrom { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
