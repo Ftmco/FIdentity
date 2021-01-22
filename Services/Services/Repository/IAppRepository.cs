9008fe8ae6b156b57438f738d17337676e86c0b7
AmirHosseinBaderan <amir0hossin0pr@gmail.com>
2021-01-22 21:08:35 +0330

    Update App Services

--- Services/Services/Repository/IAppRepository.cs
+++ Services/Services/Repository/IAppRepository.cs
@@ -1,17 +1,50 @@
 ﻿using Microsoft.AspNetCore.Http;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Services.Services.Repository
 {
+    /// <summary>
+    /// Application Repository
+    /// </summary>
     public interface IAppRepository
     {
+
+        /// <summary>
+        /// Get Application Users
+        /// </summary>
+        /// <param name="appKey">Application Id</param>
+        /// <param name="index">Index</param>
+        /// <param name="count">Show Count</param>
+        /// <returns>
+        /// Users List(IEnumerable)
+        /// </returns>
         Task<IEnumerable<Users>> GetAppUsersAsync(string appKey, int index, int count);
+
+        /// <summary>
+        /// Check Exist App 
+        /// </summary>
+        /// <param name="appKey">App Id</param>
+        /// <returns>
+        /// Exist = True
+        /// </returns>
         Task<bool> IsExistAppAsync(string appKey);
-        Task<ApplicationInfoViewModel> GetApplicationInfoAsync(string appKey,IHeaderDictionary header);
+
+        /// <summary>
+        /// Get Application Information
+        /// </summary>
+        /// <param name="appKey">App Id</param>
+        /// <param name="header">Header Request</param>
+        /// <returns>ApplicationInfoViewModel</returns>
+        Task<ApplicationInfoViewModel> GetApplicationInfoAsync(string appKey, IHeaderDictionary header);
+
+        /// <summary>
+        /// Get App Features
+        /// </summary>
+        /// <param name="appKey">App Id</param>
+        /// <returns>
+        /// List Features (IEnumerable)
+        /// </returns>
         Task<IEnumerable<AppFeatures>> GetAppFeaturesAsync(string appKey);
     }
 }
--- Services/Services/Srevices/AppServices.cs
+++ Services/Services/Srevices/AppServices.cs
@@ -7,7 +7,6 @@ using Services.Services.Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Services.Services.Srevices
