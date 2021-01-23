0f01299df89396aa7050aa0fba0e503295cd833b
AmirHosseinBaderan <amir0hossin0pr@gmail.com>
2021-01-23 23:59:52 +0330

    update signUp Service

--- FSI.Server/Pages/Account/SignUp.cshtml.cs
+++ FSI.Server/Pages/Account/SignUp.cshtml.cs
@@ -1,6 +1,8 @@
 using Fri2Ends.Identity.Services.Repository;
 using Fri2Ends.Identity.Services.Srevices;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
 
 namespace FSI.Server.Pages.Account
 {
@@ -16,10 +18,49 @@ namespace FSI.Server.Pages.Account
         }
 
         #endregion
+
         public SignupViewModel Signup { get; set; }
 
         public void OnGet()
         {
+        }
+
+        public async Task<IActionResult> OnPostSignup(SignupViewModel signup)
+        {
+            var result = await _account.SignUpAsync(signup, HttpContext.Request.Headers);
+
+            switch (result)
+            {
+                case SignUpResponse.Success:
+                    return RedirectToPage("/Login");
+                case SignUpResponse.Exception:
+                    {
+                        ViewData["Err"] = "Try Again";
+                        return Page();
+                    }
+                case SignUpResponse.UserAlreadyExist:
+                    {
+                        ViewData["Err"] = "User Already Exist";
+                        return Page();
+                    }
+                case SignUpResponse.AppNotFound:
+                    {
+                        ViewData["Err"] = "User Already Exist";
+                        return Page();
+                    }
+                case SignUpResponse.AppActivent:
+                    {
+                        ViewData["Err"] = "User Already Exist";
+                        return Page();
+                    }
+                case SignUpResponse.AppIsntForYou:
+                    {
+                        ViewData["Err"] = "User Already Exist";
+                        return Page();
+                    }
+                default:
+                    goto case SignUpResponse.Exception;
+            }
         }
     }
 }
