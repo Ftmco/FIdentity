6c33a28060d3afea1b6861bbf4d5b08bad69f947
amir hossein <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-03-30 12:29:34 -0700

    change api project

--- ClientApi/Program.cs
+++ ClientApi/Program.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace FSI.API
+namespace ClientApi
 {
     public class Program
     {
--- ClientApi/Startup.cs
+++ ClientApi/Startup.cs
@@ -12,7 +12,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace FSI.API
+namespace ClientApi
 {
     public class Startup
     {
@@ -30,7 +30,7 @@ namespace FSI.API
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "FSI.API", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ClientApi", Version = "v1" });
             });
         }
 
@@ -41,7 +41,7 @@ namespace FSI.API
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "FSI.API v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ClientApi v1"));
             }
 
             app.UseHttpsRedirection();
