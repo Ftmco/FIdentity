c5803f6230e629db441bb63727bc9e5a698c287b
amir hossein <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-04-04 14:16:36 -0700

    update components

---
+++ NPanelApplication/Server/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NPanelApplication.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        public async Task<IActionResult> Login()
+        {
+            return Ok(new
+            {
+                StatusCode = ,
+                ErrorId = 0,
+                Title = "Success",
+                Result = new { }
+            });
+        }
+    }
+}
