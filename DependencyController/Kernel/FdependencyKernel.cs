c954fe362c84310174311566d005b307bacd3299
amir hossein <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-04-04 02:44:58 -0700

    Dependency Controller

---
+++ DependencyController/Kernel/FdependencyKernel.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace FTeam.DependencyController.Kernel
+{
+    public class FdependencyKernel<TInterface, TClass> : IFdependencyKernel<TInterface, TClass> where TClass : class, TInterface, new()
+    {
+        #region ::Depdency::
+
+        /// <summary>
+        /// Interface Base For Return Injections
+        /// </summary>
+        private TInterface _interface;
+
+        #endregion
+
+        public TInterface GetDepdency(Type classType)
+        {
+            if (classType == null)
+                throw new ArgumentNullException(nameof(classType));
+
+            if (typeof(TInterface) != classType)
+                throw new TypeAccessException(nameof(classType));
+
+            return Inject(classType);
+        }
+
+        public TInterface GetDepdency()
+        {
+            if (typeof(TInterface) != typeof(TClass))
+                throw new TypeAccessException(nameof(TClass));
+
+            return Inject();
+        }
+
+        public async Task<TInterface> GetDepdencyAsync(Type classType)
+            => await Task.Run(async ()
+               =>
+               {
+                   if (classType == null)
+                       throw new ArgumentNullException(nameof(classType));
+
+                   if (typeof(TInterface) != classType)
+                       throw new TypeAccessException(nameof(classType));
+
+                   return await InjectAsync(classType);
+               });
+
+        public async Task<TInterface> GetDepdencyAsync()
+            => await Task.Run(async ()
+                =>
+                {
+                    if (typeof(TInterface) != typeof(TClass))
+                        throw new TypeAccessException(nameof(TClass));
+
+                    return await InjectAsync();
+                });
+
+        public TInterface Inject()
+        {
+            if (typeof(TInterface) != typeof(TClass))
+                throw new TypeAccessException(nameof(TClass));
+
+            return _interface = new TClass();
+        }
+
+        public TInterface Inject(Type classType)
+        {
+            if (classType == null)
+                throw new ArgumentNullException(nameof(classType));
+
+            if (typeof(TInterface) != classType)
+                throw new TypeAccessException(nameof(classType));
+
+            return _interface = new TClass();
+        }
+
+        public async Task<TInterface> InjectAsync()
+            => await Task.Run(()
+                =>
+                {
+                    if (typeof(TInterface) != typeof(TClass))
+                        throw new TypeAccessException(nameof(TClass));
+
+                    return _interface = new TClass();
+                });
+
+        public async Task<TInterface> InjectAsync(Type classType)
+            => await Task.Run(()
+                =>
+                {
+                    if (classType == null)
+                        throw new ArgumentNullException(nameof(classType));
+
+                    if (typeof(TInterface) != classType)
+                        throw new TypeAccessException(nameof(classType));
+
+                    return _interface = new TClass();
+                });
+    }
+}
---
+++ DependencyController/Kernel/IFdependencyKernel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace FTeam.DependencyController.Kernel
+{
+    public interface IFdependencyKernel<TInterface, TClass> where TClass : class, TInterface, new()
+    {
+        Task<TInterface> GetDepdencyAsync(Type classType);
+
+        Task<TInterface> GetDepdencyAsync();
+
+        TInterface GetDepdency(Type classType);
+
+        TInterface GetDepdency();
+
+        TInterface Inject();
+
+        TInterface Inject(Type classType);
+
+        Task<TInterface> InjectAsync();
+
+        Task<TInterface> InjectAsync(Type classType);
+    }
+}
