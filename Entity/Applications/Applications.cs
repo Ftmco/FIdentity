a156fb994786b4b1bef1c13d4794a39d300850d1
amir hossein <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-04-04 01:58:33 -0700

    insert applications entity

---
+++ Entity/Applications/Applications.cs
@@ -0,0 +1,46 @@
+﻿using FTeam.Entity.Sessions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace FTeam.Entity.Applications
+{
+    public record Applications
+    {
+        public Applications()
+        {
+
+        }
+
+        [Key]
+        public Guid ApplicationId { get; set; }
+
+        [Display(Name = "نام اپلیکیشن")]
+        [Required]
+        public string ApplicationName { get; set; }
+
+        [Display(Name = "ایمیل اپلیکیشن")]
+        [Required]
+        public string ApplicationEmail { get; set; }
+
+        [Display(Name = "کلمه عبور")]
+        [Required]
+        public string ApplicationPassword { get; set; }
+
+        [Display(Name = "فعال")]
+        [Required]
+        public bool IsConfirm { get; set; }
+
+        [Display(Name = "تاریخ ایجاد")]
+        [Required]
+        public DateTime CreateDate { get; set; }
+
+        [Display(Name = "ایکن اپلیکیشن")]
+        public string ApplicationIcon { get; set; }
+
+        //Relationships 
+        //Navigation Properties 
+
+        public virtual ICollection<ApplicationSessions> ApplicationSessions { get; set; }
+    }
+}
---
+++ Entity/Sessions/ApplicationSessions.cs
@@ -0,0 +1,41 @@
+﻿using FTeam.Entity.Applications;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTeam.Entity.Sessions
+{
+    public record ApplicationSessions
+    {
+        public ApplicationSessions()
+        {
+
+        }
+
+        [Key]
+        public int SessionId { get; set; }
+
+        [Required]
+        public DateTime SetDate { get; set; }
+
+        [Required]
+        public DateTime ExpireDate { get; set; }
+
+        [Required]
+        public string Token { get; set; }
+
+        [Required]
+        public string Header { get; set; }
+
+        [Required]
+        public Guid ApplicationId { get; set; }
+
+        //Relationships 
+        //Navigation Properties
+
+        public virtual Applications.Applications Applications { get; set; }
+    }
+}
